0be2e2a4ebbf60db1777a198b168450d6a456253
Makoto <mc0144832@gmail.com>
2024-11-02 13:42:42 +0900

    なんで

--- KamayamaGame/main.cpp
+++ KamayamaGame/main.cpp
@@ -9,11 +9,29 @@ int WINAPI WinMain(
 	int nCmdShow) {
 
 	Game game;
-	game.initializeWindow("釜山ゲーム", "アイコン.ico", "システムアイコン.ico", Game::SEQ_TITLE);
+	InputManager inputManager;
+	Window window(hInstance, nShowCmd, &game, &inputManager);
+	
 
-	while(!(game.terminateFlag())) {
-		game.mainloop();
+	//初期画面レンダリング
+	window.render(game.getCurrentState());
+	//初期画面表示
+	window.show();
+	mainLoop(&game, &window, &inputManager);
+
+	//終了処理
+	//WM_QUITメッセージがもう出されているので、はやく終わる。
+	termination();
+
+	return 0;
+}
+
+void mainLoop(Game* game,Window* window, InputManager* inputManager) {
+	while (window->update(game)) {// WM_QUITメッセージが受信されたらループを終了
 	}
+	//WM_QUITを受け取るとループを抜ける
+}
 
-	game.terminate();
+void termination() {//メモリ解放処理等
+	
 }
