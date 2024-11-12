b5d66dbd1a9f39260c3b7f29b08bd84297b5c8b4
Makoto <mc0144832@gmail.com>
2024-11-12 12:51:07 +0900

    将来的に複数球種を実装する

---
+++ KamayamaGame/PitchType.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+
+//‹…ŽíƒNƒ‰ƒX
+class PitchType
+{
+private:
+	std::wstring typeName;
+public:
+	PitchType(std::wstring typeName) :
+		typeName(typeName) {}
+};
+
