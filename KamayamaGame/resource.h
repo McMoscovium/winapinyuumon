7a3497b83b896b7bdc6f0ea296ca7412685e53f6
Makoto <mc0144832@gmail.com>
2024-12-10 02:58:55 +0900

    bgm追加

--- KamayamaGame/resource.h
+++ KamayamaGame/resource.h
@@ -52,12 +52,13 @@
 #define IDR_WAVE2                       148
 #define IDB_BITMAP44                    149
 #define IDB_BITMAP45                    150
+#define IDR_WAVE3                       151
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        151
+#define _APS_NEXT_RESOURCE_VALUE        152
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
