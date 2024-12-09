fe50c41d84bbcfb3b4375a858b6718bf81c47503
Makoto <mc0144832@gmail.com>
2024-12-10 03:25:21 +0900

    Audiomanagerをそろそろかんせいさせんとな

--- KamayamaGame/include/AudioManager.h
+++ KamayamaGame/include/AudioManager.h
@@ -1,5 +1,12 @@
 #pragma once
+
+#include <string>
+
 class AudioManager
 {
+private:
+public:
+	void play(const std::string& alias, const std::string& wavName, bool loop);
+	void stop(const std::string& alias);
 };
 
